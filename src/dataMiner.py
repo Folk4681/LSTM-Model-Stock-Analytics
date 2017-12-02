d6ff7600f87907da04cbbce607aadd2e8346ea4e
Vighnesh Souda <vsouda@users.noreply.github.com>
2017-12-01 20:50:10 -0500

    Grab API key from JSON file

--- src/dataMiner.py
+++ src/dataMiner.py
@@ -4,13 +4,36 @@ import sys, requests, json
 
 from pyspark.sql import SparkSession
 
+API_FILE = "../keys.json"  # specify your New York Times Archive API key here
+DATA_STORAGE = "../data/NYT_Articles"
+MONTHS = 12
+YEARS = [2016, 2017]
+baseURL = "http://api.nytimes.com/svc/archive/v1/{}/{}.json?api-key={}"
 
-API_FILE = "../keys.json" # specify your New York Times Archive API key here
-NYT_API_KEY = json.load(open(API_FILE))['New_York_Times_Archive_API']
+
+class NoFileException(Exception):
+    def __init__(self, msg=None):
+        if msg is None:
+            msg = "Error reading JSON file."
+        super(NoFileException, self).__init__(msg)
+
+
+def getAPIKey():
+    global NYT_API_KEY
+    try:
+        NYT_API_KEY = json.load(open(API_FILE))['New_York_Times_Archive_API']
+    except NoFileException as e:
+        errormsg = "Something's wrong. Perhaps your 'keys.json' file doesn't exist?"
+        raise e(errormsg)
+
+
+
 
 
 def main():
-    print (NYT_API_KEY)
+    getAPIKey()
+    print(NYT_API_KEY)
+    
 
 
 if __name__ == "__main__":
