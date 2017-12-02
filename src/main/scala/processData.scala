ac91a2ceb1b36d502bcedd3ae383da00e5eb6fa2
Vighnesh Souda <vsouda@users.noreply.github.com>
2017-12-02 18:54:51 -0500

    Set up schema for stock history, edited .gitignore, removed cache

--- src/main/scala/processData.scala
+++ src/main/scala/processData.scala
@@ -1,7 +1,7 @@
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql.functions.{array, collect_list}
 import org.apache.spark.sql.functions._
@@ -15,22 +15,33 @@ object processData {
 
     val spark = SparkSession.builder().appName("dataProcessing").master("local[*]").getOrCreate()
 
-    // For implicit conversions like converting RDDs to DataFrames
-    import spark.implicits._
 
-    val schemaTyped = new StructType()
+    /* Processing for NYT Headlines */
+    val schemaNYT = new StructType()
       .add("Headline", StringType)
       .add("Date", StringType)
 
-    val df = spark.read.option("delimiter","|").schema(schemaTyped).csv("data/NYT_ArticlesTXT/")
-    val noNullsdf = df.na.drop()
+    val NYT_Articles = spark.read.option("delimiter","|").schema(schemaNYT).csv("data/NYT_ArticlesTXT/")
+    val noNullsNYT = NYT_Articles.na.drop()
 
 
-    val dfHeadlinesGroup  = noNullsdf.groupBy("Date")
+    val NYTHeadlinesGrouped  = noNullsNYT.groupBy("Date")
       .agg(
         collect_list("Headline") as "Headlines"
       )
-    dfHeadlinesGroup.show()
+
+
+    /* Processing for Stock Symbol History */
+    val schemaStock = new StructType()
+      .add("TimeStamp", StringType)
+      .add("Open", DoubleType)
+      .add("High", DoubleType)
+      .add("Low", DoubleType)
+      .add("Close", DoubleType)
+      .add("Volume", LongType)
+
+    val STOCK_HISTORY = spark.read.option("header","true").schema(schemaStock).csv("data/Stock_Prices")
+    STOCK_HISTORY.show(false)
 
   }
 }
